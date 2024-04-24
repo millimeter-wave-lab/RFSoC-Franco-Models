d0665ddc3943abd46e4728dca312c61efe2bf622
Franco Curotto <francocurotto@gmail.com>
2024-04-24 17:37:10 -0400

    add python plot scripts

---
+++ Spectrometers/bitfiles/plot_snapshots.py
@@ -0,0 +1,64 @@
+# imports
+import argparse
+import tomli
+import calandigital as cd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# parse command line arguments
+parser = argparse.ArgumentParser(description="Plot snapshots from snapshot blocks in RFSoC model.")
+parser.add_argument("config_file", help="TOLM configuration file for script.")
+
+# main function
+def main():
+    # get config data
+    args = parser.parse_args()
+    with open(args.config_file, "rb") as f:
+        config = tomli.load(f)
+    adc_bits  = config["adc_bits"]
+    snapshots = config["snapshots"]["snap_names"]
+    n_samples = config["snapshots"]["n_samples"]
+
+    # initialize rfsoc
+    rfsoc = cd.initialize_rfsoc(config)
+
+    # create figure
+    fig, lines = create_figure(snapshots, n_samples, adc_bits)
+
+    # animation function
+    def animate(_):
+        snapdata_list = cd.read_snapshots(rfsoc, snapshots)
+        for line, snapdata in zip(lines, snapdata_list):
+            line.set_data(range(n_samples), snapdata[:n_samples])
+        return lines
+
+    # run animation
+    ani = FuncAnimation(fig, animate, blit=True, cache_frame_data=False)
+    plt.show()
+
+def create_figure(snapshots, n_samples, adc_bits):
+    """
+    Create figure with the proper axes settings for plotting snaphots.
+    """
+    axmap = {1 : (1,1), 2 : (1,2), 4 : (2,2), 16 : (4,4)}
+    n_snapshots = len(snapshots)
+
+    fig, axes = plt.subplots(*axmap[n_snapshots], squeeze=False)
+    fig.set_tight_layout(True)
+
+    lines = []
+    for snapshot, ax in zip(snapshots, axes.flatten()):
+        ax.set_xlim(0, n_samples)
+        ax.set_ylim(-2**adc_bits, 2**adc_bits)
+        ax.set_xlabel("Samples")
+        ax.set_ylabel("Amplitude")
+        ax.set_title(snapshot)
+        ax.grid()
+
+        line, = ax.plot([], [], animated=True)
+        lines.append(line)
+
+    return fig, lines
+
+if __name__ == "__main__":
+    main()
