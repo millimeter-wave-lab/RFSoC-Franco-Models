9c717415f9d6f683fca4ebea349c046539f4f8da
Franco Curotto <francocurotto@gmail.com>
2024-10-25 15:16:31 -0300

    scripts update

--- Multiband-Receiver/MBR_experiment/multiband_save_hotcold.py
+++ Multiband-Receiver/MBR_experiment/multiband_save_hotcold.py
@@ -1,5 +1,6 @@
 import tomllib
 import numpy as np
+import matplotlib.pyplot as plt
 
 import sys
 sys.path.append("../..")
@@ -21,18 +22,25 @@ def main():
     invert_reg   = config["multiband"]["invert_reg"]
     invert_delay = config["multiband"]["invert_delay"]
     comb_brams   = config["multiband"]["comb_brams"]
+    lo_freq1     = config["dss1"]["lo_freq"]
+    lo_freq2     = config["dss2"]["lo_freq"]
 
     # useful parameters
     n_brams = len(comb_brams)
     dtype   = ">u" + str(data_width//8)
     n_bins  = 2**addr_width * n_brams 
     freqs   = np.linspace(0, bandwidth, n_bins, endpoint=False)
+    rf_freqs_usb1  = lo_freq1 + freqs/1e3 # GHz
+    rf_freqs_lsb1  = lo_freq1 - freqs/1e3
+    rf_freqs_usb2  = lo_freq2 + freqs/1e3 # GHz
+    rf_freqs_lsb2  = lo_freq2 - freqs/1e3
 
     # invert parameters
     combined_bin = (2**addr_width-invert_delay) * n_brams
     combined_freq = freqs[combined_bin]
     uncombined_freqs = freqs[:combined_bin]
     combined_freqs = freqs[combined_bin:]
+    rf_freqs_comb = lo_freq1 + combined_freqs/1e3
 
     # initialize rfsoc
     rfsoc = cd.initialize_rfsoc(config)
@@ -57,6 +65,7 @@ def main():
     b2_lsb_cold = cd.read_interleave_data(rfsoc, synth_band2[0], addr_width, data_width, dtype)
     b2_usb_cold = cd.read_interleave_data(rfsoc, synth_band2[1], addr_width, data_width, dtype)
     combined_cold = cd.read_interleave_data(rfsoc, comb_brams, addr_width, data_width, dtype)
+    combined_cold = np.flip(combined_cold[combined_bin:])
     print("done")
 
     input("Set the load to hot and press Enter")
@@ -66,12 +75,111 @@ def main():
     b2_lsb_hot = cd.read_interleave_data(rfsoc, synth_band2[0], addr_width, data_width, dtype)
     b2_usb_hot = cd.read_interleave_data(rfsoc, synth_band2[1], addr_width, data_width, dtype)
     combined_hot = cd.read_interleave_data(rfsoc, comb_brams, addr_width, data_width, dtype)
+    combined_hot = np.flip(combined_hot[combined_bin:])
     print("done")
+
+
+    # Y computation
+    Y_lsb1 = b1_lsb_hot / b1_lsb_cold
+    Y_usb1 = b1_usb_hot / b1_usb_cold
+    Y_lsb2 = b2_lsb_hot / b2_lsb_cold
+    Y_usb2 = b2_usb_hot / b2_usb_cold
+    Y_comb = combined_hot / combined_cold
+
+    # filtering
+    Y_lsb1[Y_lsb1 < 1.1] = 1.1
+    Y_usb1[Y_usb1 < 1.1] = 1.1
+    Y_lsb2[Y_lsb2 < 1.1] = 1.1
+    Y_usb2[Y_usb2 < 1.1] = 1.1
+    Y_comb[Y_comb < 1.1] = 1.1
+
+    # noise figure computation
+    enr_lsb1 = 14.76
+    enr_usb1 = 14.84
+    enr_lsb2 = 14.92
+    enr_usb2 = 14.96
+    enr_comb = 14.88
+    Nf_lsb1 = enr_lsb1 - 10*np.log10(Y_lsb1 - 1)
+    Nf_usb1 = enr_usb1 - 10*np.log10(Y_usb1 - 1)
+    Nf_lsb2 = enr_lsb2 - 10*np.log10(Y_lsb2 - 1)
+    Nf_usb2 = enr_usb2 - 10*np.log10(Y_usb2 - 1)
+    Nf_comb = enr_comb - 10*np.log10(Y_comb - 1)
+
+    # temperature computation
+    Nt_lsb1 = 290 * (np.power(10, Nf_lsb1/10) - 1)
+    Nt_usb1 = 290 * (np.power(10, Nf_usb1/10) - 1)
+    Nt_lsb2 = 290 * (np.power(10, Nf_lsb2/10) - 1)
+    Nt_usb2 = 290 * (np.power(10, Nf_usb2/10) - 1)
+    Nt_comb = 290 * (np.power(10, Nf_comb/10) - 1)
 
     print("Saving data...", end="", flush=True)
     np.savez("multiband_hot_cold.npz",
-             b1_lsb_cold, b1_usb_cold, b2_lsb_cold, b2_usb_cold, combined_cold,
-             b1_lsb_hot,  b1_usb_hot,  b2_lsb_hot,  b2_usb_hot,  combined_hot)
+             b1_lsb_cold   = b1_lsb_cold, 
+             b1_usb_cold   = b1_usb_cold,
+             b2_lsb_cold   = b2_lsb_cold,
+             b2_usb_cold   = b2_usb_cold,
+             combined_cold = combined_cold,
+             b1_lsb_hot    = b1_lsb_hot, 
+             b1_usb_hot    = b1_usb_hot, 
+             b2_lsb_hot    = b2_lsb_hot,
+             b2_usb_hot    = b2_usb_hot, 
+             combined_hot  = combined_hot,
+             Nt_lsb1       = Nt_lsb1,
+             Nt_usb1       = Nt_usb1,
+             Nt_lsb2       = Nt_lsb2,
+             Nt_usb2       = Nt_usb2,
+             Nt_comb       = Nt_comb,
+             rf_freqs_usb1 = rf_freqs_usb1,
+             rf_freqs_lsb1 = rf_freqs_lsb1,
+             rf_freqs_usb2 = rf_freqs_usb2,
+             rf_freqs_lsb2 = rf_freqs_lsb2,
+             rf_freqs_comb = rf_freqs_comb)
+
+    # plot hotcold
+    plt.figure()
+    # B1 LSB cold
+    spec_data = cd.scale_and_dBFS_specdata(b1_lsb_cold, acc_len, dBFS)
+    plt.plot(rf_freqs_lsb1, spec_data, "b")
+    # B1 USB cold
+    spec_data = cd.scale_and_dBFS_specdata(b1_usb_cold, acc_len, dBFS)
+    plt.plot(rf_freqs_usb1, spec_data, "b")
+    # B2 LSB cold
+    spec_data = cd.scale_and_dBFS_specdata(b2_lsb_cold, acc_len, dBFS)
+    plt.plot(rf_freqs_lsb2, spec_data, "b")
+    # B2 USB cold
+    spec_data = cd.scale_and_dBFS_specdata(b2_usb_cold, acc_len, dBFS)
+    plt.plot(rf_freqs_usb2, spec_data, "b")
+    # combined band cold
+    spec_data = cd.scale_and_dBFS_specdata(combined_cold, acc_len, dBFS)
+    plt.plot(rf_freqs_comb, spec_data)
+    # B1 LSB hot
+    spec_data = cd.scale_and_dBFS_specdata(b1_lsb_hot, acc_len, dBFS)
+    plt.plot(rf_freqs_lsb1, spec_data, "r")
+    # B1 USB hot
+    spec_data = cd.scale_and_dBFS_specdata(b1_usb_hot, acc_len, dBFS)
+    plt.plot(rf_freqs_usb1, spec_data, "r")
+    # B2 LSB hot
+    spec_data = cd.scale_and_dBFS_specdata(b2_lsb_hot, acc_len, dBFS)
+    plt.plot(rf_freqs_lsb2, spec_data, "r")
+    # B2 USB hot
+    spec_data = cd.scale_and_dBFS_specdata(b2_usb_hot, acc_len, dBFS)
+    plt.plot(rf_freqs_usb2, spec_data, "r")
+    # combined band hot
+    spec_data = cd.scale_and_dBFS_specdata(combined_hot, acc_len, dBFS)
+    plt.plot(rf_freqs_comb, spec_data)
+    plt.savefig("hotcold.png")
+
+    # plot temperature
+    plt.figure()
+    plt.plot(rf_freqs_lsb1, Nt_lsb1)
+    plt.plot(rf_freqs_usb1, Nt_usb1)
+    plt.plot(rf_freqs_lsb2, Nt_lsb2)
+    plt.plot(rf_freqs_usb2, Nt_usb2)
+    plt.plot(rf_freqs_comb, Nt_comb)
+    plt.ylim((0, 2000))
+    plt.savefig("noise_temp.png")
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
